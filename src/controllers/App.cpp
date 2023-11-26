038136671cfcfc5627c72ff4fd13c77552bf4653
Jordan Levy <jordan.levy@bigspark.dev>
2023-11-26 11:33:42 +0000

    rendering calls in App

--- src/controllers/App.cpp
+++ src/controllers/App.cpp
@@ -1,4 +1,5 @@
 #include "controllers/App.h"
+#include "Tetrimino.h"
 
 #include <iostream>
 #include <chrono>
@@ -132,6 +133,9 @@ void App::Run()
 
     /* --------- Initial State --------- */
     registry->LoadScene("../res/conf/example.yaml");
+    std::unordered_map<TetriminoShape, glm::mat4> tetriminoMap = Tetrimino::LoadTetriminos("../res/conf/tetriminos.yaml");
+    std::shared_ptr<RenderComponent> cubeComp = std::make_shared<RenderComponent>("../res/shaders/Lighting.shader", "../res/models/cube.obj");
+    Tetrimino test = Tetrimino(tetriminoMap[TetriminoShape::T], cubeComp);
 
     while (!glfwWindowShouldClose(windowManager->window))
     {
