6d55bb4a66f95e19b992adc0e632be63aaeff870
Jordan Levy <jordan.levy@bigspark.dev>
2023-12-02 19:15:19 +0000

    ScriptSystem basic implementation

---
+++ res/scripts/TetrisGrid.lua
@@ -0,0 +1,13 @@
+-- TetrisGrid
+
+TetrisGrid = {
+    ready = function(self)
+        print("Initializing TetrisGrid!")
+    end,
+
+    process = function(self, delta)
+        print("TetrisGrid process")
+    end
+}
+
+print("TetrisGrid script!")
